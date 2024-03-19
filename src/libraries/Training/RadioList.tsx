d2240c994a006f0d81f695ae5aae5f1cb44f56c7
AmitKashid11 <amitkashid0000@gmail.com>
2024-03-19 13:57:04 +0530

    commit

--- src/libraries/Training/RadioList.tsx
+++ src/libraries/Training/RadioList.tsx
@@ -1,10 +1,11 @@
-import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
+import { FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
 import { ErrorDetail } from '../styled/ErrormessageStyled';
 
 const RadioList = ({ ItemList, ClickItem, Label, DefaultValue, ErrorMessage = '' }) => {
     console.log(ErrorMessage);
 
     return (<>
+        <Typography>{Label}</Typography>
         <RadioGroup value={DefaultValue} onChange={(e) => ClickItem(e.target.value)}>
             <div>
                 {ItemList?.map((Item, i) => {
